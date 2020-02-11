c341c20f610abc9cdd2b42bb3a4ec1a8c9f4d9a8
Anil <anil@gutentor.com>
2020-02-11 19:21:30 +0545

    Added : CosmosWP WoCommerce demo 1

---
+++ includes/template-add/templates/class-woocommerce.php
@@ -0,0 +1,97 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+if ( ! class_exists( 'Cosmoswp_Template_Library_Agency' ) ) {
+
+    /**
+     * Functions related to About Block
+     * @package CosmosWP
+     * @since 1.0.0
+     *
+     */
+
+    class Cosmoswp_Template_Library_Agency extends Cosmoswp_Template_Library_Base{
+        
+        /**
+         * Gets an instance of this object.
+         * Prevents duplicate instances which avoid artefacts and improves performance.
+         *
+         * @static
+         * @access public
+         * @since 1.0.0
+         * @return object
+         */
+        public static function get_instance() {
+
+            // Store the instance locally to avoid private static replication
+            static $instance = null;
+
+            // Only run these methods if they haven't been ran previously
+            if ( null === $instance ) {
+                $instance = new self();
+            }
+
+            // Always return the instance
+            return $instance;
+
+        }
+
+        /**
+         * Load block library
+         * Used for blog template loading
+         *
+         * @since      1.0.0
+         * @package    CosmosWP
+         * @author     CosmosWP <info@cosmoswp.com>
+         *
+         * @param $templates_list array
+         * @return array
+         */
+        public function add_demo_template_library( $templates_list ){
+
+            $demo_list = array(
+                
+                array(
+                    'title' => __( 'WooCommerce 1', 'cosmoswp' ),/*Title*/
+                    'is_premium' => false,/*Premium*/
+                    'type' => 'gutentor',/*Optional eg gutentor, elementor or other page builders*/
+                    'author' => __( 'CosmosWP', 'cosmoswp' ),/*Author Name*/
+                    'keywords' => array( 'main', 'demo' ),/*Search keyword*/
+                    'categories' => array( 'WooCommerce','multipurpose' ),/*Categories*/
+                    'template_url' => array(
+                        'content' => COSMOSWP_TEMPLATE_LIBRARY_URL.'includes/template-data/woocommerce/shop-1/content.json',
+                        'options' => COSMOSWP_TEMPLATE_LIBRARY_URL.'includes/template-data/woocommerce/shop-1/options.json',
+                        'widgets' => COSMOSWP_TEMPLATE_LIBRARY_URL.'includes/template-data/woocommerce/shop-1/widgets.json'
+                    ),
+                    'screenshot_url' => COSMOSWP_TEMPLATE_LIBRARY_URL.'includes/template-data/woocommerce/shop-1/screenshot.jpg',/*Screenshot of block*/
+
+                    'demo_url' => 'https://www.demo.cosmoswp.com/demo-4/',/*Demo Url*/
+                    'plugins' => array(
+                        array(
+                            'name'      => 'Gutentor',
+                            'slug'      => 'gutentor',
+                        ),
+                        array(
+                            'name'      => 'Breadcrumb NavXT',
+                            'slug'      => 'breadcrumb-navxt',
+                        ),
+                        array(
+                            'name'      => 'Contact Form 7',
+                            'slug'      => 'contact-form-7',
+                            'main_file' => 'wp-contact-form-7.php',
+                        ),
+                        array(
+                            'name'      => 'WooCommerce',
+                            'slug'      => 'woocommerce',
+                        ),
+                    )
+                ),
+            );
+
+            return array_merge( $templates_list, $demo_list );
+        }
+    }
+}
+Cosmoswp_Template_Library_Agency::get_instance()->run();
