7d641486f740afdfee52e5991bf608381981f404
Anil <anil@gutentor.com>
2020-03-13 14:24:20 +0545

    added fitness pro template data

--- includes/template-add/templates/class-fitness.php
+++ includes/template-add/templates/class-fitness.php
@@ -79,6 +79,29 @@ if ( ! class_exists( 'Cosmoswp_Template_Library_Fitness' ) ) {
                         ),
                     )
                 ),
+                108 => array(
+                    'title' => __( 'Fitness', 'cosmoswp' ),/*Title*/
+                    'is_pro' => true,/*Premium*/
+                    'type' => 'gutentor',/*Optional eg gutentor, elementor or other page builders*/
+                    'author' => __( 'CosmosWP', 'cosmoswp' ),/*Author Name*/
+                    'keywords' => array( 'main', 'demo' ),/*Search keyword*/
+                    'categories' => array( 'fitness' ),/*Categories*/
+            
+                    'screenshot_url' => COSMOSWP_TEMPLATE_LIBRARY_URL.'includes/template-data/fitness/fitness-pro-1/screenshot.jpg',/*Screenshot of block*/
+
+                    'demo_url' => 'https://www.demo.cosmoswp.com/demo-108/',/*Demo Url*/
+                    'plugins' => array(
+                        array(
+                            'name'      => 'Gutentor',
+                            'slug'      => 'gutentor',
+                        ),
+                        array(
+                            'name'      => 'Contact Form 7',
+                            'slug'      => 'contact-form-7',
+                            'main_file' => 'wp-contact-form-7.php',
+                        ),
+                    )
+                ),
             );
 
             return array_merge( $templates_list, $demo_list );
