41710e6664833dd1b8307835dbe68bbf45a8c36d
cosmoswp <cosmoswpofficial@gmail.com>
2020-05-26 15:07:07 +0545

    Updated: Edd template

--- includes/template-add/templates/class-edd.php
+++ includes/template-add/templates/class-edd.php
@@ -53,7 +53,7 @@ if ( ! class_exists( 'Cosmoswp_Template_Library_EDD' ) ) {
 
             $demo_list = array(
                 
-                'demo-4' => array(
+                'demo-16' => array(
                     'title' => __( 'EDD 1', 'cosmoswp' ),/*Title*/
                     'is_pro' => false,/*Premium*/
                     'type' => 'gutentor',/*Optional eg gutentor, elementor or other page builders*/
