6ce7587102b8cf013b0eff83421af91e74511786
cosmoswp <cosmoswpofficial@gmail.com>
2020-06-19 15:13:51 +0545

    Added : Blog Magazine 1 Template Demo Data

--- includes/template-add/templates/class-blog.php
+++ includes/template-add/templates/class-blog.php
@@ -79,6 +79,38 @@ if ( ! class_exists( 'Cosmoswp_Template_Library_Blog' ) ) {
                         ),
                     )
                 ),
+                
+                'demo-17' => array(
+                    'title' => __( 'Magazine 1', 'cosmoswp' ),/*Title*/
+                    'is_pro' => false,/*Premium*/
+                    'type' => 'gutentor',/*Optional eg gutentor, elementor or other page builders*/
+                    'author' => __( 'CosmosWP', 'cosmoswp' ),/*Author Name*/
+                    'keywords' => array( 'main', 'demo' ),/*Search keyword*/
+                    'categories' => array( 'blog','magazine' ),/*Categories*/
+                    'template_url' => array(
+                        'content' => COSMOSWP_TEMPLATE_LIBRARY_URL.'includes/template-data/blog/magazine-1/content.json',
+                        'options' => COSMOSWP_TEMPLATE_LIBRARY_URL.'includes/template-data/blog/magazine-1/options.json',
+                        'widgets' => COSMOSWP_TEMPLATE_LIBRARY_URL.'includes/template-data/blog/magazine-1/widgets.json'
+                    ),
+                    'screenshot_url' => COSMOSWP_TEMPLATE_LIBRARY_URL.'includes/template-data/blog/magazine-1/screenshot.jpg',/*Screenshot of block*/
+
+                    'demo_url' => 'https://www.demo.cosmoswp.com/demo-17/',/*Demo Url*/
+                    'plugins' => array(
+                        array(
+                            'name'      => 'Gutentor',
+                            'slug'      => 'gutentor',
+                        ),
+                        array(
+                            'name'      => 'Contact Form 7',
+                            'slug'      => 'contact-form-7',
+                            'main_file' => 'wp-contact-form-7.php',
+                        ),
+                        array(
+                            'name'      => 'Breadcrumb NavXT',
+                            'slug'      => 'breadcrumb-navxt',
+                        ),
+                    )
+                ),
                 'demo-111' => array(
                     'title' => __( 'Blog 1', 'cosmoswp' ),/*Title*/
                     'is_pro' => true,/*Premium*/
@@ -90,6 +122,33 @@ if ( ! class_exists( 'Cosmoswp_Template_Library_Blog' ) ) {
                     'screenshot_url' => COSMOSWP_TEMPLATE_LIBRARY_URL.'includes/template-data/blog/blog-pro-1/screenshot.jpg',/*Screenshot of block*/
 
                     'demo_url' => 'https://www.demo.cosmoswp.com/demo-111/',/*Demo Url*/
+                    'plugins' => array(
+                        array(
+                            'name'      => 'Gutentor',
+                            'slug'      => 'gutentor',
+                        ),
+                        array(
+                            'name'      => 'Breadcrumb NavXT',
+                            'slug'      => 'breadcrumb-navxt',
+                        ),
+                        array(
+                            'name'      => 'Contact Form 7',
+                            'slug'      => 'contact-form-7',
+                            'main_file' => 'wp-contact-form-7.php',
+                        ),
+                    )
+                ),
+                'demo-113' => array(
+                    'title' => __( 'Magazine 1', 'cosmoswp' ),/*Title*/
+                    'is_pro' => true,/*Premium*/
+                    'pro_url' => 'https://www.cosmoswp.com/pricing/',/*Pro url*/
+                    'type' => 'gutentor',/*Optional eg gutentor, elementor or other page builders*/
+                    'author' => __( 'CosmosWP', 'cosmoswp' ),/*Author Name*/
+                    'keywords' => array( 'main', 'demo' ),/*Search keyword*/
+                    'categories' => array( 'blog' ),/*Categories*/
+                    'screenshot_url' => COSMOSWP_TEMPLATE_LIBRARY_URL.'includes/template-data/blog/magazine-pro-1/screenshot.jpg',/*Screenshot of block*/
+
+                    'demo_url' => 'https://www.demo.cosmoswp.com/demo-117/',/*Demo Url*/
                     'plugins' => array(
                         array(
                             'name'      => 'Gutentor',
